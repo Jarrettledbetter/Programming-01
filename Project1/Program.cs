c30a7cf3c5920916c39822115a1c87998ff3b314
Jarrettledbetter <Jarrettledbetter@gmail.com>
2022-01-17 21:43:06 -0600

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        //adding two numbers +
+        /* comments */
+        //define
+        static void Main(string[] args)
+        {
+
+            //define variable
+            int a = 5;
+            int b = 3;
+           
+
+            //convert one type to another in order to convert typecasting 
+            //type casting ()
+            int c = a + b ;
+            int d = a - b ;
+            int e = a * b ;
+            int f = a / b ;
+            //Console.WriteLine("This is my first program!");
+            Console.WriteLine("Enter value of a:{0}", a);
+            Console.WriteLine("Enter value of b:{0}", b);
+            Console.WriteLine("Sum of the two numbers is {0}", c);
+            Console.WriteLine("Difference is : {0}", d);
+            Console.WriteLine("Mul is : {0}", e);
+            Console.WriteLine("Div is : {0}", f);
+
+
+        }
+    }
+}   // Enter the value of a : 5
+    // Enter next value b : 3
+    // Sum : 8
+    // Difference : 2
+    // Mul : 15
+    // Div : 1
